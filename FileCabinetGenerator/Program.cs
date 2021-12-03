4ff1d3eccd1548154a141610375f270c2c83cdc3
alexandergamezo <alexandergamezo@gmail.com>
2021-12-03 12:31:01 +0300

    Add initial version of FileCabinetGenerator.

---
+++ FileCabinetGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCabinetGenerator
+{
+    static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
