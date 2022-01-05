d569949f5e412336a2be5990272d65b35254af7a
alexandergamezo <alexandergamezo@gmail.com>
2022-01-05 11:32:36 +0300

    Applied template 'Chain of Responsibility'

---
+++ FileCabinetApp/CommandHandlers/CommandHandlerBase.cs
@@ -0,0 +1,38 @@
+﻿namespace FileCabinetApp.CommandHandlers
+{
+    /// <summary>
+    /// Base handler class.
+    /// </summary>
+    public class CommandHandlerBase : ICommandHandler
+    {
+        private ICommandHandler nextHandler;
+
+        /// <summary>
+        /// Сonnects handlers.
+        /// </summary>
+        /// <param name="handler">The next handler in a chain.</param>
+        /// <returns>Returns the next handler in a chain.</returns>
+        public ICommandHandler SetNext(ICommandHandler handler)
+        {
+            this.nextHandler = handler;
+            return handler;
+        }
+
+        /// <summary>
+        /// Executes a request.
+        /// </summary>
+        /// <param name="request">Command request.</param>
+        /// <returns>Method for executing a request.</returns>
+        public virtual object Handle(AppCommandRequest request)
+        {
+            if (this.nextHandler != null)
+            {
+                return this.nextHandler.Handle(request);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
